2a62091d39420f95f68cc727789e964017af7fae
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-03-05 23:14:21 +0600

    two pointer

--- 125.valid-palindrome.js
+++ 125.valid-palindrome.js
@@ -9,12 +9,16 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    s= s.replace(/[^a-zA-Z0-9]/g, '',/\s/g,).toLowerCase()
-   let reverse= s.split('').reverse().join('')
-   if(s===reverse) return true
-   return false
- 
+var isPalindrome = function (s) {
+  s = s.replace(/[^a-zA-Z0-9]/g, '', /\s/g).toLowerCase();
+    let p1 = 0,
+    p2 = s.length - 1;
+  if (s.length <= 1) return true;
+  while (p1 < p2) {
+    if (s[p1] !== s[p2]) return false;
+    p1++;
+    p2--;
+  }
+  return true;
 };
 // @lc code=end
-
