1af355091e4a337136203547b35463d695dbe0ef
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-03-07 13:06:59 +0600

    first solution

---
+++ 5.longest-palindromic-substring.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=5 lang=javascript
+ *
+ * [5] Longest Palindromic Substring
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+var longestPalindrome = function(s) {
+    let maxSub = '';
+    const bubbleFromCenter = (left, right) => {
+        while(left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+        return s.slice(left+1, right)
+    }
+    for(let i = 0; i < s.length; i++) {
+        const sub1 = bubbleFromCenter(i, i);
+        const sub2 = bubbleFromCenter(i, i+1);
+        const sub = sub1.length > sub2.length ? sub1 : sub2
+        if(sub.length > maxSub.length) {
+            maxSub = sub
+        }
+    }
+    return maxSub
+};
+// @lc code=end
