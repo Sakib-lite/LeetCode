75f09d61f8463347708a9f4363763b09fae167e2
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-03-07 14:03:26 +0600

    cleaned up

--- 5.longest-palindromic-substring.js
+++ 5.longest-palindromic-substring.js
@@ -9,24 +9,21 @@
  * @param {string} s
  * @return {string}
  */
-
-var longestPalindrome = function(s) {
-    let maxSub = '';
-    const bubbleFromCenter = (left, right) => {
-        while(left >= 0 && right < s.length && s[left] === s[right]) {
-            left--;
-            right++;
-        }
-        return s.slice(left+1, right)
-    }
-    for(let i = 0; i < s.length; i++) {
-        const sub1 = bubbleFromCenter(i, i);
-        const sub2 = bubbleFromCenter(i, i+1);
-        const sub = sub1.length > sub2.length ? sub1 : sub2
-        if(sub.length > maxSub.length) {
-            maxSub = sub
-        }
-    }
-    return maxSub
+ var longestPalindrome = function(s) {
+  let longest = '';
+  for(let i = 0; i < s.length; i++) {
+      bubble(i, i); 
+      bubble(i, i+1); 
+  }
+  function bubble(left, right) {
+      while(left >= 0 && s[left] === s[right]) {
+          left--;
+          right++;
+      }
+      left++;
+      right--;
+      if(longest.length < right-left+1)  longest = s.slice(left, right+1)
+  }
+  return longest;
 };
 // @lc code=end
