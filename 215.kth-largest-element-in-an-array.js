160c33ea2aa03f160f7e7c13a86d52a75c88f521
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-04-07 14:37:14 +0600

    solved with quick sort algorithm. time complexity 0(n log n)

--- 215.kth-largest-element-in-an-array.js
+++ 215.kth-largest-element-in-an-array.js
@@ -10,14 +10,37 @@
  * @param {number} k
  * @return {number}
  */
-var findKthLargest = function(nums, k) {
-    nums=nums.sort((x, y) =>x-y)
-    let j=nums.length-1-k;
-let largest=nums[nums.length-1];
-    for(let i=nums.length-1; i>j; i--){
-largest=Math.min(largest,nums[i]);
+
+function swapNumbers(arr, first, second) {
+  const temp = arr[first];
+  arr[first] = arr[second];
+  arr[second] = temp;
 }
-return largest
+
+function pivotFunction(arr, pivot = 0, end = arr.length - 1) {
+  let swapIndex = pivot;
+  for (let i = pivot + 1; i <= end; i++) {
+    swapIndex++;
+    swapNumbers(arr, i, swapIndex, i);
+  }
+  return swapIndex;
+}
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivot = pivotFunction(arr, left, right);
+    quickSort(arr, left, pivot - 1);
+    quickSort(arr, pivot + 1, right);
+  }
+  return arr;
+}
+var findKthLargest = function (nums, k) {
+  nums = nums.sort((x, y) => x - y);
+  let arr = quickSort(nums);
+  let j = nums.length - 1 - k;
+  let largest = nums[nums.length - 1];
+  for (let i = nums.length - 1; i > j; i--) {
+    largest = Math.min(largest, nums[i]);
+  }
+  return largest;
 };
 // @lc code=end
-
