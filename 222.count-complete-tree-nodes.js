d8a84bbd9163cfe7dcdcfd6901b9e999c755024f
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-04-13 14:42:29 +0600

    solved with binary search algorithm

--- 222.count-complete-tree-nodes.js
+++ 222.count-complete-tree-nodes.js
@@ -18,17 +18,46 @@
  * @return {number}
  */
 
-const binaryTree = function (node, arr) {
-  let temp = node;
-  arr.push(temp.val);
-  if (temp.left) binaryTree(temp.left,arr);
-  if (temp.right) binaryTree(temp.right,arr);
+const getTreeHeight = function (root) {
+  let height = 0;
+  while (root.left !== null) {
+    height++;
+    root = root.left;
+  }
+  return height;
 };
 
-var countNodes = function (root) {
+const nodeExists = function (idxToFind, height, node) {
+  let left = 0,
+    right = Math.pow(2, height) - 1,
+    count = 0;
+  while (count < height) {
+    const midOfNode = Math.ceil((left + right) / 2);
+    if (idxToFind >= midOfNode) {
+      node = node.right;
+      left = midOfNode;
+    } else {
+      node = node.left;
+      right = midOfNode - 1;
+    }
+
+    count++;
+  }
+  return node !== null;
+};
+
+const countNodes = function (root) {
   if (!root) return 0;
-  let result = [];
-  binaryTree(root, result);
-  return result.length;
+  const height = getTreeHeight(root);
+  if (height === 0) return 1;
+  const upperCount = Math.pow(2, height) - 1;
+  let left = 0,
+    right = upperCount;
+  while (left < right) {
+    const idxToFind = Math.ceil((left + right) / 2);
+    if (nodeExists(idxToFind, height, root))  left = idxToFind;
+    else  right = idxToFind - 1;
+  }
+  return upperCount + left + 1;
 };
 // @lc code=end
