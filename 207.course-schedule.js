bfe1f541ad6f1f744ebd23ab9d21cc8ea054bb69
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-04-23 16:56:00 +0600

    solved with topological sort. time: O(p+n^2).  space: O(n^2)

--- 207.course-schedule.js
+++ 207.course-schedule.js
@@ -11,42 +11,46 @@
  * @return {boolean}
  */
 var canFinish = function (numCourses, prerequisites) {
-  const adjList = new Array(numCourses).fill(0).map(() => []);
+  const inDegree = new Array(numCourses).fill(0);
+  const adjList = inDegree.map(() => []);
   for (let i = 0; i < prerequisites.length; i++) {
     const val = prerequisites[i];
+    inDegree[val[0]]++;
     adjList[val[1]].push(val[0]);
   }
-  for (let v = 0; v < numCourses; v++) {
-    const queue = [],
-      seen = {};
-    for (let i = 0; i < adjList[v].length; i++) {
-      queue.push(adjList[v][i]);
-      while (queue.length) {
-        const current = queue.shift();
-        seen[current] = true;
-        if (current === v) return false;
-        const adjaecent = adjList[current];
-        for (let i = 0; i < adjaecent.length; i++) {
-          if (!seen[adjaecent[i]]) {
-            queue.push(adjaecent[i]);
-          }
-        }
+
+  const stack = [];
+  for (let v = 0; v < inDegree.length; v++) {
+    if (inDegree[v] === 0) stack.push(v);
+  }
+  let count = 0;
+
+  while (stack.length) {
+    const current = stack.pop();
+    count++;
+    const adjaecent = adjList[current];
+    for (let i = 0; i < adjaecent.length; i++) {
+      const next = adjaecent[i];
+      inDegree[next]--;
+      if (inDegree[next] === 0) {
+        stack.push(next);
       }
     }
   }
-  return true;
+
+  return count === numCourses;
 };
 
-// console.log(
-//   canFinish(7, [
-//     [1, 0],
-//     [2, 1],
-//     [2, 5],
-//     [0, 3],
-//     [4, 3],
-//     [3, 5],
-//     [4, 5],
-//   ])
-// );
+console.log(
+  canFinish(7, [
+    [1, 0],
+    [2, 1],
+    [2, 5],
+    [0, 3],
+    [4, 3],
+    [3, 5],
+    [4, 5],
+  ])
+);
 
 // @lc code=end
