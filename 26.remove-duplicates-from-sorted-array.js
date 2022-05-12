f0e06be20577165ac62f9f88ba4334ca6bc1d7a8
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-05-12 13:34:49 +0600

    solved with two pointer

--- 26.remove-duplicates-from-sorted-array.js
+++ 26.remove-duplicates-from-sorted-array.js
@@ -1,26 +1,24 @@
+/*
+ * @lc app=leetcode id=26 lang=javascript
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
 // @lc code=start
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  let count = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    console.log(i)
-    if (nums[i] != nums[i - 1]) {
-        console.log(nums[i], nums[i - 1]);
-      nums[count] = nums[i];
-      count++;
-    }
-     console.log(count)
-   console.log(nums) 
-}
-
-//   [0,1,2,3,4,2,2,3,3]
-  return count;
+  let i = 0,
+    j = i + 1;
+  while (j < nums.length) {
+    if (nums[i] !== nums[j]) {
+      i++;
+      nums[i] = nums[j];
+      j++;
+    } else j++;
+  }
+  return i + 1;
 };
-
-const arr = removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
-console.log(arr);
 // @lc code=end
