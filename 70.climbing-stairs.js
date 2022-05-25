7cbf596183258defc5cdeb2e2f484f375c4ade90
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-05-25 12:58:46 +0600

    memorization

---
+++ 70.climbing-stairs.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=70 lang=javascript
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  const memo = [1, 1, 2];
+  for (let i = 3; i <= n; i++) memo[i] = memo[i - 1] + memo[i - 2];
+  return memo[n];
+};
+// @lc code=end
