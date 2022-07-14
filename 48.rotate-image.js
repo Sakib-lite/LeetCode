bb89dc6eabc65824e67191091183596b25be5ee4
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-07-14 11:50:58 +0600

    rotate the matrix in 90 degree angle

---
+++ 48.rotate-image.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=48 lang=javascript
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = i; j < matrix[0].length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+  for (let row of matrix) {
+    row.reverse();
+  }
+};
+
+// @lc code=end
