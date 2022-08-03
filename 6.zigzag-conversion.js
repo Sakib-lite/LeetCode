66d198da696db5d6d909b574d0aa512ad9c27793
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-08-03 13:07:10 +0600

    zigzag conversion

---
+++ 6.zigzag-conversion.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=6 lang=javascript
+ *
+ * [6] Zigzag Conversion
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+  if (numRows === 1 || s.length < numRows) return s;
+  let rows = [],
+    count = 0,
+    reverse = false;
+
+  for (let i = 0; i < numRows; i++) rows[i] = [];
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    rows[count].push(char);
+    reverse ? count-- : count++;
+    if (count === numRows - 1 || count === 0) reverse = !reverse;
+  }
+  return rows.reduce((res, cur) => (res += cur.join('')), '');
+};
+
+// @lc code=end
