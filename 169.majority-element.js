53e59414e69c6586579e66fc8145f8843a1d51a4
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-09-16 19:02:25 +0600

    more optimal solution

--- 169.majority-element.js
+++ 169.majority-element.js
@@ -11,11 +11,9 @@
  */
 var majorityElement = function (nums) {
   const hash = {};
-  let max = 0;
   for (let i = 0; i < nums.length; i++) {
     hash[nums[i]] = hash[nums[i]] + 1 || 1;
-    max = Math.max(max, hash[nums[i]]);
+    if (hash[nums[i]] > nums.length / 2) return nums[i];
   }
-  return Object.keys(hash).find((key) => hash[key] === max);
 };
 // @lc code=end
