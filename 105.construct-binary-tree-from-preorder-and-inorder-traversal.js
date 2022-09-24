197d19a1e2e3799369b82a3dbf2bea68edc6a53e
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-09-24 11:18:29 +0600

    tree

---
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=105 lang=javascript
+ *
+ * [105] Construct Binary Tree from Preorder and Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
+  if (!preorder.length || !inorder.length) return null;
+
+  const root = new TreeNode(preorder[0]);
+  const mid = inorder.indexOf(root.val);
+
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+
+  return root;
+};
+// @lc code=end
