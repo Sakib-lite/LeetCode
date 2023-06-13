618b826f8952f3f2b356ccd9155aa7840aa20aa0
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2023-06-13 09:34:34 +0600

    solved

--- 4.median-of-two-sorted-arrays.js
+++ 4.median-of-two-sorted-arrays.js
@@ -11,37 +11,14 @@
  * @return {number}
  */
 
-function pivotFunction(arr, pivot = 0, right = arr.length - 1) {
-  let swapIndex = pivot;
-  for (let i = pivot + 1; i <= right; i++) {
-    if (arr[i] < arr[pivot]) {
-      swapIndex++;
-      [arr[swapIndex], arr[i]] = [arr[i], arr[swapIndex]];
-    }
-  }
-  [arr[pivot], arr[swapIndex]] = [arr[swapIndex], arr[pivot]];
-  return swapIndex;
-}
-
-function quickSort(arr, left = 0, right = arr.length - 1) {
-  if (left < right) {
-    let p = pivotFunction(arr, left, right);
-    quickSort(arr, left, p - 1);
-    quickSort(arr, p + 1, right);
-  }
-  return arr;
-}
-
 var findMedianSortedArrays = function (nums1, nums2) {
-  let arr = [...nums1, ...nums2];
-  quickSort(arr, 0, arr.length - 1);
-  if (arr.length % 2 === 0) {
-    let mid = arr.length / 2;
-    console.log(mid);
-    return (arr[mid] + arr[mid - 1]) / 2;
-  } else {
-    let mid = Math.floor(arr.length / 2);
-    return arr[mid];
+  const mergedArr = [...nums1, ...nums2].sort((a, b) => a - b);
+  const mid = Math.floor(mergedArr.length / 2);
+
+  if (mergedArr.length % 2 === 0) {
+    return (mergedArr[mid - 1] + mergedArr[mid]) / 2;
   }
+
+  return mergedArr[mid];
 };
 // @lc code=end
