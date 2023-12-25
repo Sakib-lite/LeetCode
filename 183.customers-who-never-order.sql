2e6aeb784974223642e1e3f82f9173162665a013
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2023-12-25 17:09:49 +0600

    more efficient solution

--- 183.customers-who-never-order.sql
+++ 183.customers-who-never-order.sql
@@ -7,13 +7,15 @@
 -- @lc code=start
 # Write your MySQL query statement below
 
-SELECT
-	c.NAME AS Customers 
-FROM
-	Customers c
-	LEFT JOIN Orders o ON c.id = o.customerId 
-WHERE
-	o.id IS NULL
+-- SELECT
+-- 	c.NAME AS Customers 
+-- FROM
+-- 	Customers c
+-- 	LEFT JOIN Orders o ON c.id = o.customerId 
+-- WHERE
+-- 	o.id IS NULL
+
+SELECT c.name as Customers FROM `Customers` c WHERE c.id NOT IN (SELECT o.customerId FROM Orders o) 
 
 
 -- @lc code=end
